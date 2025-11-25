e0e24d00250cefd6ccee789d3128ebb32e2258a9
scontreraslopez <s.contreraslopez@edu.gva.es>
2025-11-25 17:08:46 +0100

    pantalla conectada

--- app/src/main/java/net/iessochoa/sergiocontreras/thedogwalker/ui/DogWalkerApp.kt
+++ app/src/main/java/net/iessochoa/sergiocontreras/thedogwalker/ui/DogWalkerApp.kt
@@ -1,14 +1,21 @@
 package net.iessochoa.sergiocontreras.thedogwalker.ui
 
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import net.iessochoa.sergiocontreras.thedogwalker.data.DogRepository
+import net.iessochoa.sergiocontreras.thedogwalker.ui.screens.DogListScreen
 
 @Composable
 fun DogWalkerApp() {
     Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
+        DogListScreen(
+            DogRepository.getAllDogs(),
+            {},
+            modifier = Modifier.fillMaxSize().padding(innerPadding)
+        )
 
     }
 }
--- app/src/main/java/net/iessochoa/sergiocontreras/thedogwalker/ui/screens/DogListScreen.kt
+++ app/src/main/java/net/iessochoa/sergiocontreras/thedogwalker/ui/screens/DogListScreen.kt
@@ -18,7 +18,6 @@ import androidx.compose.material.icons.filled.Face
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -29,8 +28,12 @@ import net.iessochoa.sergiocontreras.thedogwalker.ui.components.StatusIconsRow
 import net.iessochoa.sergiocontreras.thedogwalker.ui.theme.Typography
 
 @Composable
-fun DogListScreen(dogs: List<Dog>, onDogClick: (Int) -> Unit) {
-    LazyColumn(contentPadding = PaddingValues(16.dp)) {
+fun DogListScreen(
+    dogs: List<Dog>,
+    onDogClick: (Int) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    LazyColumn(contentPadding = PaddingValues(16.dp), modifier = modifier) {
         items(dogs) { dog ->
             Card(
                 modifier = Modifier.fillMaxWidth().padding(8.dp).clickable { onDogClick(dog.id) },
