92313bbd2860f8763266d066fa785932b05359ba
scontreraslopez <s.contreraslopez@edu.gva.es>
2025-11-25 16:57:03 +0100

    imports ready

--- app/src/main/java/net/iessochoa/sergiocontreras/thedogwalker/ui/screens/DogList.kt
+++ app/src/main/java/net/iessochoa/sergiocontreras/thedogwalker/ui/screens/DogList.kt
@@ -1,18 +1,32 @@
 package net.iessochoa.sergiocontreras.thedogwalker.ui.screens
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Face
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import net.iessochoa.sergiocontreras.thedogwalker.model.Dog
+import net.iessochoa.sergiocontreras.thedogwalker.ui.components.StatusIconsRow
+import net.iessochoa.sergiocontreras.thedogwalker.ui.theme.Typography
 
 @Composable
 fun DogListScreen(dogs: List<Dog>, onDogClick: (Int) -> Unit) {
@@ -27,8 +41,8 @@ fun DogListScreen(dogs: List<Dog>, onDogClick: (Int) -> Unit) {
                     Box(modifier = Modifier.size(60.dp)) { Icon(Icons.Default.Face, null, Modifier.fillMaxSize()) }
                     Spacer(Modifier.width(16.dp))
                     Column(modifier = Modifier.weight(1f)) {
-                        Text(text = dog.name, style = MaterialTheme.typography.titleMedium)
-                        Text(text = dog.breed, style = MaterialTheme.typography.bodyMedium)
+                        Text(text = dog.name, style = Typography.bodyMedium)
+                        Text(text = dog.breed, style = Typography.bodyMedium)
                     }
                     // Iconos de estado (Solo lectura)
                     StatusIconsRow(dog, clickable = false)
