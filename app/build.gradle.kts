0b3fee9fd263ff03e4d79f5f5144b7a2246ad79d
a <a>
2025-11-29 11:37:32 +0100

    v1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.kotlin.dsl.implementation
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -42,6 +44,13 @@ android {
 }
 
 dependencies {
+
+    val nav_version = "2.9.6"
+
+    // Jetpack Compose integration
+    implementation(libs.androidx.navigation.compose)
+
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
